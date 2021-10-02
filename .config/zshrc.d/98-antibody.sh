01d800e363ee04709c40c4c804cc9fde3b804c60
Elliana L. Perry <elliana.perry@gmail.com>
2021-10-01 22:02:06 -0400

    test

--- .config/zshrc.d/98-antibody.sh
+++ .config/zshrc.d/98-antibody.sh
@@ -3,18 +3,18 @@ if [[ ! -f "$HOME/.local/bin/antibody" ]]; then
 fi
 
 antibody bundle << PLUGINEOF > ~/.config/zsh_plugins.sh
-zsh-users/zsh-completions
-zsh-users/zsh-history-substring-search
-zsh-users/zsh-syntax-highlighting
-zsh-users/zsh-autosuggestions 
-greymd/docker-zsh-completion
-jeanpantoja/dotpyvenv
 ohmyzsh/ohmyzsh path:plugins/colorize
 ohmyzsh/ohmyzsh path:plugins/sudo
 ohmyzsh/ohmyzsh path:plugins/git
 ohmyzsh/ohmyzsh path:plugins/fzf
 ohmyzsh/ohmyzsh path:plugins/dirhistory
 ohmyzsh/ohmyzsh path:plugins/zsh_reload
+zsh-users/zsh-completions
+zsh-users/zsh-history-substring-search
+zsh-users/zsh-syntax-highlighting
+zsh-users/zsh-autosuggestions 
+greymd/docker-zsh-completion
+jeanpantoja/dotpyvenv
 PLUGINEOF
 
 
