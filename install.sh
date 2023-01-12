a7e9b74aed2fc5c99fe80c70aabef64379370308
Elliana Perry <elliana.perry@gmail.com>
2023-01-12 09:49:04 -0500

    (feat): added install script for devcontainers

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Try to detect where the ditfiles have been cloned to
+if [[ -d "${HOME}/.dotfiles" ]]; then
+  DOTFILES="${HOME}/.dotfiles"
+elif [[ -d "${HOME}/dotfiles" ]]; then
+  DOTFILES="${HOME}/dotfiles"
+else
+  echo "Could not locate dotfile clone directory"
+  exit 1
+fi
+# Stow dotfiles if stow is available
+if which stow 2>/dev/null; then
+  stow --dir="${HOME}/.dotfiles" --target="${HOME}/" --restow home
+else
+  echo "GNU/Stow was not found in path or is otherwise not available"
+  exit 1
+fi
