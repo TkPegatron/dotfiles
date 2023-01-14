c4717280b776c029eb3cca61a530f61891f3a6b3
Elliana Perry <elliana.perry@gmail.com>
2023-01-14 10:29:39 -0500

    additional handling for vscode

--- install.sh
+++ install.sh
@@ -10,8 +10,12 @@ else
 fi
 # Stow dotfiles if stow is available
 if which stow 2>/dev/null; then
-  stow --dir="${DOTFILES}/" --target="${HOME}/" --restow --ignore=".gitconfig" home
+  stow --dir="${DOTFILES}/" --target="${HOME}/" --restow --verbose --ignore=".gitconfig" home
 else
   echo "GNU/Stow was not found in path or is otherwise not available"
   exit 1
+fi
+if [[ "${TERM_PROGRAM}" == 'vscode' ]] && [[ "$REMOTE_CONTAINERS" ]]; then
+  echo "vscode detected, linking gpg"
+  stow --dir="${HOME}/.gnupg/" --target="${HOME}/.config/gnupg" --restow --verbose .
 fi
